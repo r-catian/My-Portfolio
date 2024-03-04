e23f86ec7a79c94adea4ad8fc606087b3db8dc32
ryan <catianryanmark@gmail.com>
2024-03-04 22:06:41 +0900

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+function openNav() {
+  document.getElementById("myNav").style.width = "60%";
+  document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
